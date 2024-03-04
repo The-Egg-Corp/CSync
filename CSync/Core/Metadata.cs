7d72daf70e765feaf90e98d7bedf97d5d3c4878c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-04 11:36:14 +0000

    remove metadata class

--- CSync/Core/Metadata.cs
+++
@@ -1,7 +0,0 @@
-namespace CSync.Core;
-
-static class Metadata {
-    public const string GUID = "io.github.CSync";
-    public const string NAME = "CSync";
-    public const string VERSION = "2.1.0";
-}
