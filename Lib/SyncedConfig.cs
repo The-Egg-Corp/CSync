f1500596feebfd83f9d71e083e798a6d91451560
O3H <owen77stubbs@gmail.com>
2024-03-04 14:12:40 +0000

    Update SyncedConfig.cs

--- Lib/SyncedConfig.cs
+++ Lib/SyncedConfig.cs
@@ -34,6 +34,11 @@ public class SyncedConfig<T>(string guid) : SyncedInstance<T>, ISynchronizable w
 
     internal SyncedEntry<bool> SYNC_TO_CLIENTS { get; private set; } = null;
 
+    /// <summary>
+    /// Allow the host to control whether clients can use their own config.
+    /// This MUST be called after binding the entry parameter.
+    /// </summary>
+    /// <param name="hostSyncControlOption">The entry for the host to use in your config file.</param>
     protected void EnableHostSyncControl(SyncedEntry<bool> hostSyncControlOption) {
         SYNC_TO_CLIENTS = hostSyncControlOption;
 
