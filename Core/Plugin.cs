384117cae0f4fbaf2cef384ef08be3295233c147
O3H <owen77stubbs@gmail.com>
2024-03-09 09:26:15 +0000

    fix plugin doc

--- Core/Plugin.cs
+++ Core/Plugin.cs
@@ -7,10 +7,8 @@ using System;
 namespace CSync;
 
 /// <summary>
-/// The plugin and main entry point of this library, responsible for holding the logger.<br></br>
-/// Helps with internal logging and caching of config files through static references.<br></br>
-/// <br></br>
-/// Do <b>NOT</b> use this class directly or create any instances of it!
+/// The main entry point of this library.<br></br>
+/// Holds a reference to the logger and applies patches on load.
 /// </summary>
 [BepInPlugin(GUID, NAME, VERSION)]
 public class Plugin : BaseUnityPlugin {
