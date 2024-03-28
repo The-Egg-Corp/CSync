d93ad0722d6cea3138cff12f6459f4e2b750d855
O3H <owen77stubbs@gmail.com>
2024-03-28 01:12:47 +0000

    Create LethalConfigPatch.cs

---
+++ Patches/LethalCompany/LethalConfigPatch.cs
@@ -0,0 +1,20 @@
+using HarmonyLib;
+
+using UnityEngine.UI;
+
+namespace CSync.Patches.LethalCompany;
+
+[HarmonyPatch(typeof(Button))]
+internal class LethalConfigPatch {
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Button.OnPointerClick))]
+    static void ApplyClicked(Button __instance) {
+        var isLC = __instance.transform.parent.name == "BottomBar";
+        if (!isLC) return;
+
+        var isApply = __instance.transform.name == "ApplyButton";
+        if (!isApply) return;
+
+        // If we are the host, make all clients resync.
+    }
+}
