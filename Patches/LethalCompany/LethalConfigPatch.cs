a610c70af228186cdb0a769a0fe08dcf18b2ca99
O3H <owen77stubbs@gmail.com>
2024-04-06 08:20:57 +0100

    brub

--- Patches/LethalCompany/LethalConfigPatch.cs
+++ Patches/LethalCompany/LethalConfigPatch.cs
@@ -8,13 +8,13 @@ internal class LethalConfigPatch {
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Button.OnPointerClick))]
     static void ApplyClicked(Button __instance) {
-        Plugin.Logger.LogDebug("Apply clicked! Attempting to resync..");
-        
         var isLC = __instance.transform.parent.name == "BottomBar";
         if (!isLC) return;
 
         var isApply = __instance.transform.name == "ApplyButton";
         if (!isApply) return;
+
+        Plugin.Logger.LogDebug("Apply clicked! Attempting to resync..");
 
         // If we are the host, make all clients resync.
         //if (NetworkManager.Singleton.IsHost) {
